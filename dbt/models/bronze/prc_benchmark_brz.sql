6b7c7947ab35a1cc5730634ae88ceb710f59364d
rihab-lab <rihab.bahri7@gmail.com>
2025-05-23 09:58:40 +0200

    feat: create bronze tables

--- dbt/models/bronze/prc_benchmark_brz.sql
+++ dbt/models/bronze/prc_benchmark_brz.sql
@@ -1,20 +1,21 @@
+-- dbt/models/bronze/prc_benchmark_brz.sql
+
+-- On ajoute la PK en post-hook (métadonnée Snowflake)
 {{ config(
     materialized = "table",
     schema       = "BRONZE_LAYER",
     post_hook    = [
-      -- Snowflake n’applique pas physiquement la PK, mais on la stocke en métadonnée
       "alter table {{ this }} add primary key (APUKCode, Anabench2Code)"
     ]
 ) }}
 
--- CTAS vide : on ne crée que la structure, sans lignes
 select
-  cast(null as varchar) as APUKCode,            -- NOT NULL
-  cast(null as varchar) as Anabench2Code,      -- NOT NULL
-  cast(null as varchar) as Anabench2,
-  cast(null as varchar) as SKUGroup,
-  cast(null as varchar) as SYS_SOURCE_DATE,
-  cast(null as timestamp_ltz) as CREATE_DATE,  -- Copy into metadata
-  cast(null as varchar)   as file_name,        -- Copy into metadata
+  cast(null as varchar(16777216)) as APUKCode,
+  cast(null as varchar(16777216)) as Anabench2Code,
+  cast(null as varchar(16777216)) as Anabench2,
+  cast(null as varchar(16777216)) as SKUGroup,
+  cast(null as varchar(16777216)) as SYS_SOURCE_DATE,
+  cast(null as timestamp_ltz)     as CREATE_DATE,
+  cast(null as varchar(16777216)) as file_name,
   cast(current_timestamp() as timestamp_ltz) as LOAD_TIME
 where false
