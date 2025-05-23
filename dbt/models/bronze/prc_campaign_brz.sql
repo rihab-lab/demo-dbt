a263a66a264dd38fa1eeb715932e468600b42412
rihab-lab <rihab.bahri7@gmail.com>
2025-05-23 10:19:09 +0200

    feat: create bronze tables

---
+++ dbt/models/bronze/prc_campaign_brz.sql
@@ -0,0 +1,23 @@
+-- dbt/models/bronze/prc_campaign_brz.sql
+
+{{ config(
+    materialized = "table",
+    schema       = "BRONZE_LAYER",
+    post_hook    = [
+      "alter table {{ this }} add primary key (HOUSEKEY, CAMPAIGNCODE)"
+    ]
+) }}
+
+select
+  cast(null as varchar(16777216))    as HOUSEKEY,                     -- NOT NULL
+  cast(null as varchar(16777216))    as CAMPAIGNCODE,                 -- NOT NULL
+  cast(null as varchar(16777216))    as CAMPAIGNNAME,
+  cast(null as varchar(16777216))    as CAMPAIGNDESCRIPTION,
+  cast(null as varchar(16777216))    as HISTORICALSELLINFIRSTMONTH,
+  cast(null as varchar(16777216))    as HISTORICALSELLINLASTMONTH,
+  cast(null as varchar(16777216))    as CAMPAIGNDATE,
+  cast(null as varchar(16777216))    as SYS_SOURCE_DATE,
+  cast(null as timestamp_ltz)        as CREATE_DATE,                  -- metadata COPY INTO
+  cast(null as varchar(16777216))    as file_name,                    -- metadata COPY INTO
+  cast(current_timestamp() as timestamp_ltz) as LOAD_TIME
+where false
