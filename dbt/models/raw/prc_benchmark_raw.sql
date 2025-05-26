9328e546e45b590fe7271cf76216ac56fa82eb4b
rihab-lab <rihab.bahri7@gmail.com>
2025-05-26 15:09:50 +0200

    feat: dbt azure to raw

--- dbt/models/raw/prc_benchmark_raw.sql
+++ dbt/models/raw/prc_benchmark_raw.sql
@@ -1,20 +1,23 @@
+-- models/raw/prc_benchmark_raw.sql
+
+-- On ne rechargera jamais deux fois un même fichier grâce à unique_key
 {{ 
   config(
     materialized = "incremental",
     database     = "TEST_POC_VISEO_DB",
     schema       = "RAW_LAYER",
-    unique_key   = "FILE_NAME"   -- on ne rechargera jamais deux fois un même fichier
+    unique_key   = "FILE_NAME"
   )
 }}
 
 with staged as (
   select
-    $1::varchar(16777216)                         as APUKCode,
-    $2::varchar(16777216)                         as Anabench2Code,
-    $3::varchar(16777216)                         as Anabench2,
-    $4::varchar(16777216)                         as SKUGroup,
-    metadata$filename                             as FILE_NAME,
-    metadata$created_on::timestamp_ltz            as SYS_SOURCE_DATE
+    $1::varchar(16777216)              as APUKCode,
+    $2::varchar(16777216)              as Anabench2Code,
+    $3::varchar(16777216)              as Anabench2,
+    $4::varchar(16777216)              as SKUGroup,
+    metadata$filename                  as FILE_NAME,
+    metadata$created_on::timestamp_ltz as SYS_SOURCE_DATE
   from @{{ this.database }}.{{ this.schema }}.EXTERNAL_AZURE_STAGE
   where metadata$filename like 'PRC_BENCHMARK_%'
 )
@@ -22,7 +25,6 @@ with staged as (
 select * from staged
 
 {% if is_incremental() %}
-  -- sur les runs suivants, on ne prend que les fichiers plus récents que le max déjà chargé
   where SYS_SOURCE_DATE > (
     select max(SYS_SOURCE_DATE) from {{ this }}
   )
