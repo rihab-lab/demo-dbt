34e057f8811a8823dccce1d62ed7fdf57d2df157
rihab-lab <rihab.bahri7@gmail.com>
2025-05-27 10:52:09 +0200

    feat: dbt azure to raw

--- dbt/macros/create_all_pipes.sql
+++ dbt/macros/create_all_pipes.sql
@@ -13,10 +13,25 @@
         {% set qualified_pipe_name = database ~ '.' ~ schema ~ '.' ~ pipe.name %}
         {% set qualified_table = database ~ '.' ~ schema ~ '.' ~ pipe.table %}
         {% set qualified_stage = database ~ '.' ~ schema ~ '.' ~ pipe.stage %}
+
+        {% set file_columns = pipe.columns %}
+        {% set all_columns = file_columns + ['FILE_NAME', 'LOAD_TIME'] %}
+
+        {% set copy_into_columns %}
+            ({{ all_columns | join(', ') }})
+        {% endset %}
+
+        {% set select_expr %}
+            {{ file_columns | map('regex_replace', '^', 't.$') | join(', ') }}, metadata$filename, current_timestamp()
+        {% endset %}
+
         {% set sql %}
             create or replace pipe {{ qualified_pipe_name }} as
-            copy into {{ qualified_table }}
-            from @{{ qualified_stage }}
+            copy into {{ qualified_table }} {{ copy_into_columns }}
+            from (
+              select {{ select_expr }}
+              from @{{ qualified_stage }} t
+            )
             pattern = '{{ pipe.pattern }}'
             file_format = (type = csv field_optionally_enclosed_by='"' skip_header=1);
         {% endset %}
