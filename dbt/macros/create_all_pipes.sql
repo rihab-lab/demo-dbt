28248183a213624cb12a277f7df394fd3719310b
rihab-lab <rihab.bahri7@gmail.com>
2025-05-27 09:43:22 +0200

    feat: dbt azure to raw

--- dbt/macros/create_all_pipes.sql
+++ dbt/macros/create_all_pipes.sql
@@ -1,11 +1,22 @@
 {% macro create_all_pipes_from_config() %}
     {% set pipes = var('pipe_configs', []) %}
     {% do log("Nombre de pipes à créer : " ~ pipes | length, info=True) %}
+
+    {% set messages = [] %}
+
     {% for pipe in pipes %}
-        create or replace pipe {{ pipe.name }} as
-        copy into {{ pipe.table }}
-        from @{{ pipe.stage }}
-        pattern = '{{ pipe.pattern }}'
-        file_format = (type = csv field_optionally_enclosed_by='"' skip_header=1);
+        {% set sql %}
+            create or replace pipe {{ pipe.name }} as
+            copy into {{ pipe.table }}
+            from @{{ pipe.stage }}
+            pattern = '{{ pipe.pattern }}'
+            file_format = (type = csv field_optionally_enclosed_by='"' skip_header=1);
+        {% endset %}
+
+        {% do log("Exécution SQL : " ~ sql, info=True) %}
+        {% do run_query(sql) %}
+        {% do messages.append("PIPE " ~ pipe.name ~ " créé") %}
     {% endfor %}
+
+    {% do return(messages) %}
 {% endmacro %}
