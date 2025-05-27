d62c386b8a44b9d25c635b5dd2c957d9de20b91d
rihab-lab <rihab.bahri7@gmail.com>
2025-05-27 14:58:42 +0200

    feat: dbt raw to bronze

--- dbt/macros/generate_bronze_model.sql
+++ dbt/macros/generate_bronze_model.sql
@@ -1,10 +1,12 @@
 {% macro generate_bronze_merge_model(pipe_table_name, unique_key) %}
-    {% set config_dict = var('pipe_configs')
+    {% set config_dict = (
+        var('pipe_configs')
         | selectattr("table", "equalto", pipe_table_name)
         | list
-        | first %}
+        | first
+    ) %}
 
-    {# enrichir les colonnes avec les métadonnées si non déjà présentes #}
+    {# Enrichir les colonnes avec les métadonnées si non déjà présentes #}
     {% set required_columns = ['FILE_NAME', 'LOAD_TIME'] %}
     {% set columns = config_dict.columns %}
 
@@ -24,12 +26,9 @@
     ) }}
 
     select
-        {{ columns | join(',\n        ') }}
-    from {{ var('SF_DATABASE') }}.{{ var('SF_SCHEMA') }}.{{ config_dict.table }}
+        {{ columns | join(',\n    ') }}
+    from {{ source('RAW_LAYER', pipe_table_name) }}
     {% if is_incremental() %}
-      where LOAD_TIME > (
-        select coalesce(max(LOAD_TIME), '1900-01-01'::timestamp_ltz)
-        from {{ this }}
-      )
+      where LOAD_TIME > (select max(LOAD_TIME) from {{ this }})
     {% endif %}
 {% endmacro %}
