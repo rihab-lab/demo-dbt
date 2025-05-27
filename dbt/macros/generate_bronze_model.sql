7e57fad441f524308b22592c5faf1e433f4cc9ee
rihab-lab <rihab.bahri7@gmail.com>
2025-05-27 15:07:26 +0200

    feat: dbt raw to bronze

--- dbt/macros/generate_bronze_model.sql
+++ dbt/macros/generate_bronze_model.sql
@@ -1,34 +1,35 @@
 {% macro generate_bronze_merge_model(pipe_table_name, unique_key) %}
-    {% set config_dict = (
-        var('pipe_configs')
-        | selectattr("table", "equalto", pipe_table_name)
-        | list
-        | first
-    ) %}
+  {% set config_dict = (
+      var('pipe_configs')
+      | selectattr("table", "equalto", pipe_table_name)
+      | list
+      | first
+  ) %}
 
-    {# Enrichir les colonnes avec les métadonnées si non déjà présentes #}
-    {% set required_columns = ['FILE_NAME', 'LOAD_TIME'] %}
-    {% set columns = config_dict.columns %}
-
-    {% for col in required_columns %}
-        {% if col not in columns %}
-            {% do columns.append(col) %}
-        {% endif %}
-    {% endfor %}
-
-    {{ config(
-        materialized = 'incremental',
-        incremental_strategy = 'merge',
-        unique_key = unique_key,
-        schema = 'BRONZE_LAYER',
-        database = var('SF_DATABASE'),
-        on_schema_change = 'append_new_columns'
-    ) }}
-
-    select
-        {{ columns | join(',\n    ') }}
-    from {{ source('RAW_LAYER', pipe_table_name) }}
-    {% if is_incremental() %}
-      where LOAD_TIME > (select max(LOAD_TIME) from {{ this }})
+  {# Ajout des métadonnées si non déjà présentes #}
+  {% set required_columns = ['FILE_NAME', 'LOAD_TIME'] %}
+  {% set columns = config_dict.columns %}
+  {% for col in required_columns %}
+    {% if col not in columns %}
+      {% do columns.append(col) %}
     {% endif %}
+  {% endfor %}
+
+  {{ config(
+      materialized='incremental',
+      incremental_strategy='merge',
+      unique_key=unique_key,
+      schema='BRONZE_LAYER',
+      database=var('SF_DATABASE'),
+      on_schema_change='append_new_columns'
+  ) }}
+
+  select
+    {{ columns | join(',\n    ') }}
+  from {{ var('SF_DATABASE') }}.{{ var('SF_SCHEMA') }}.{{ pipe_table_name }}
+  {% if is_incremental() %}
+    where (
+      select count(*) from {{ this }}
+    ) = 0 or LOAD_TIME > (select max(LOAD_TIME) from {{ this }})
+  {% endif %}
 {% endmacro %}
