b8992473319661703c3a315702d6e02f40050992
rihab-lab <rihab.bahri7@gmail.com>
2025-05-27 13:56:13 +0200

    feat: dbt raw to bronze

--- dbt/macros/generate_bronze_model.sql
+++ dbt/macros/generate_bronze_model.sql
@@ -4,22 +4,32 @@
         | list
         | first %}
 
+    {# enrichir les colonnes avec les métadonnées si non déjà présentes #}
+    {% set required_columns = ['FILE_NAME', 'LOAD_TIME'] %}
+    {% set columns = config_dict.columns %}
+
+    {% for col in required_columns %}
+        {% if col not in columns %}
+            {% do columns.append(col) %}
+        {% endif %}
+    {% endfor %}
+
     {{ config(
         materialized = 'incremental',
         incremental_strategy = 'merge',
         unique_key = unique_key,
         schema = 'BRONZE_LAYER',
         database = var('SF_DATABASE'),
-        post_hook = [
-            "alter table {{ this }} add primary key (" ~ unique_key | join(', ') ~ ")"
-        ],
         on_schema_change = 'append_new_columns'
     ) }}
 
     select
-        {{ config_dict.columns | join(',\n        ') }}
+        {{ columns | join(',\n        ') }}
     from {{ var('SF_DATABASE') }}.{{ var('SF_SCHEMA') }}.{{ config_dict.table }}
     {% if is_incremental() %}
-      where LOAD_TIME > (select max(LOAD_TIME) from {{ this }})
+      where LOAD_TIME > (
+        select coalesce(max(LOAD_TIME), '1900-01-01'::timestamp_ltz)
+        from {{ this }}
+      )
     {% endif %}
 {% endmacro %}
