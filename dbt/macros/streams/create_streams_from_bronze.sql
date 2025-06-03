a1efc81428962e9de13d09301eaf225f1a381785
rihab-lab <rihab.bahri7@gmail.com>
2025-06-03 10:51:53 +0200

    fix: correct stream creation

--- dbt/macros/streams/create_streams_from_bronze.sql
+++ dbt/macros/streams/create_streams_from_bronze.sql
@@ -1,9 +1,5 @@
 {% macro create_streams_from_bronze() %}
-  {% set bronze_tables = [
-    'PRC_BENCHMARK_BRZ',
-    'PRC_CAMPAIGN_BRZ'
-  ] %}
-
+  {% set bronze_tables = ['PRC_BENCHMARK_BRZ', 'PRC_CAMPAIGN_BRZ'] %}
   {% set schema = 'BRONZE_LAYER' %}
   {% set database = var('SF_DATABASE') %}
   {% set results = [] %}
@@ -11,27 +7,34 @@
   {% for table in bronze_tables %}
     {% set stream_name = table ~ '_STREAM' %}
 
-    {# Liste tous les streams dans le schéma concerné #}
-    {% set show_streams_query %}
-      show streams in schema {{ database }}.{{ schema }}
+    {% set check_query %}
+      select count(*) as count
+      from {{ database }}.information_schema.streams
+      where stream_name = upper('{{ stream_name }}')
+        and table_name = upper('{{ table }}')
+        and schema_name = upper('{{ schema }}')
     {% endset %}
 
-    {% set show_result = run_query(show_streams_query) %}
-    {% set stream_names = show_result.columns[0].values() %}
-    {% set exists = stream_name in stream_names %}
+    {% set check_result = run_query(check_query) %}
+    {% if execute %}
+      {% set count = check_result.columns[0].values()[0] %}
+    {% else %}
+      {% set count = 0 %}
+    {% endif %}
 
-    {% if not exists %}
-      {% set sql %}
+    {% if count == 0 %}
+      {% set create_query %}
         create or replace stream {{ database }}.{{ schema }}.{{ stream_name }}
         on table {{ database }}.{{ schema }}.{{ table }}
         append_only = false;
       {% endset %}
-
-      {% do log("réation du stream : " ~ stream_name, info=True) %}
-      {% do run_query(sql) %}
-      {% do results.append("Stream créé : " ~ stream_name) %}
+      
+      {% do run_query(create_query) %}
+      {% do log("Stream créé : " ~ stream_name, info=True) %}
+      {% do results.append("Créé : " ~ stream_name) %}
     {% else %}
-      {% do log("Stream déjà existant : " ~ stream_name ~ ", ignoré", info=True) %}
+      {% do log("Stream déjà existant : " ~ stream_name, info=True) %}
+      {% do results.append("Déjà existant : " ~ stream_name) %}
     {% endif %}
   {% endfor %}
 
