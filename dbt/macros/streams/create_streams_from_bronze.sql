5ebeb47dfef62f8109ff946a3be0f1969982118e
rihab-lab <rihab.bahri7@gmail.com>
2025-06-03 11:29:41 +0200

    fix: correct stream creation

--- dbt/macros/streams/create_streams_from_bronze.sql
+++ dbt/macros/streams/create_streams_from_bronze.sql
@@ -1,39 +1,39 @@
 {% macro create_streams_from_bronze() %}
-  {% set bronze_tables = ['PRC_BENCHMARK_BRZ', 'PRC_CAMPAIGN_BRZ'] %}
+  {% set bronze_tables = [
+    'PRC_BENCHMARK_BRZ',
+    'PRC_CAMPAIGN_BRZ'
+  ] %}
+
   {% set schema = 'BRONZE_LAYER' %}
   {% set database = var('SF_DATABASE') %}
   {% set results = [] %}
 
   {% for table in bronze_tables %}
     {% set stream_name = table ~ '_STREAM' %}
-    {% set stream_exists = false %}
 
-    {% set show_query %}
+    {# Liste tous les streams dans le schéma concerné #}
+    {% set show_streams_query %}
       show streams in schema {{ database }}.{{ schema }}
     {% endset %}
-    {% set show_result = run_query(show_query) %}
 
-    {% for row in show_result.rows %}
-      {% if row[1] is not none and row[1] | upper == stream_name %}
-        {% set stream_exists = true %}
-      {% endif %}
-    {% endfor %}
+    {% set show_result = run_query(show_streams_query) %}
+    {% set stream_names = show_result.columns[0].values() %}
+    {% set exists = stream_name in stream_names %}
 
-    {% if not stream_exists %}
+    {% if not exists %}
       {% set sql %}
         create or replace stream {{ database }}.{{ schema }}.{{ stream_name }}
         on table {{ database }}.{{ schema }}.{{ table }}
         append_only = false;
       {% endset %}
 
-      {% do log("✅ Création du stream : " ~ stream_name, info=True) %}
+      {% do log("création du stream : " ~ stream_name, info=True) %}
       {% do run_query(sql) %}
       {% do results.append("Stream créé : " ~ stream_name) %}
     {% else %}
-      {% do log("ℹ️ Stream déjà existant : " ~ stream_name ~ ", ignoré", info=True) %}
-      {% do results.append("Stream ignoré : " ~ stream_name) %}
+      {% do log("Stream déjà existant : " ~ stream_name ~ ", ignoré", info=True) %}
     {% endif %}
   {% endfor %}
 
   {% do return(results) %}
-{% endmacro %}
+{% endmacro %} 
