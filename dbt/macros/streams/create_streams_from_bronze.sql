5176df5ed32df326b16ed35ba5bec72ee460af83
rihab-lab <rihab.bahri7@gmail.com>
2025-06-03 11:04:18 +0200

    fix: correct stream creation

--- dbt/macros/streams/create_streams_from_bronze.sql
+++ dbt/macros/streams/create_streams_from_bronze.sql
@@ -1,31 +1,24 @@
 {% macro create_streams_from_bronze() %}
-  {% set bronze_tables = [
-    'PRC_BENCHMARK_BRZ',
-    'PRC_CAMPAIGN_BRZ'
-  ] %}
-
+  {% set bronze_tables = ['PRC_BENCHMARK_BRZ', 'PRC_CAMPAIGN_BRZ'] %}
   {% set schema = 'BRONZE_LAYER' %}
   {% set database = var('SF_DATABASE') %}
   {% set results = [] %}
 
   {% for table in bronze_tables %}
     {% set stream_name = table ~ '_STREAM' %}
+    {% set stream_exists = false %}
 
-    {# Show streams from the schema #}
     {% set show_query %}
       show streams in schema {{ database }}.{{ schema }}
     {% endset %}
 
     {% set show_result = run_query(show_query) %}
-    {% set stream_exists = false %}
-
-    {% if show_result %}
-      {% for row in show_result.rows %}
-        {% if row['name'] | upper == stream_name %}
-          {% set stream_exists = true %}
-        {% endif %}
-      {% endfor %}
-    {% endif %}
+    
+    {% for row in show_result.rows %}
+      {% if row['name'] | upper == stream_name %}
+        {% set stream_exists = true %}
+      {% endif %}
+    {% endfor %}
 
     {% if not stream_exists %}
       {% set create_sql %}
@@ -39,6 +32,7 @@
       {% do results.append("Stream créé : " ~ stream_name) %}
     {% else %}
       {% do log("ℹ️ Stream déjà existant : " ~ stream_name ~ ", ignoré", info=True) %}
+      {% do results.append("Stream ignoré : " ~ stream_name) %}
     {% endif %}
   {% endfor %}
 
